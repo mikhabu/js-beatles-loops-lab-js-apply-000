32c20b9d772a6333e96c5ce6fd58a57b5d7f3e97
Mikhail Buslau <mikhailbuslau@gmail.com>
2020-03-25 17:41:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ function theBeatlesPlay(arrMusician, arrInstrument) {
   let newArr = [];
   for (let i = 0; i < arrMusician.length; i++) {
     let curMusician = arrMusician[i];
-    for (let j = 0; j < arrInstrument.length; j++) {
-      let curInstrument = arrInstrument[j];
+    let curInstrument = arrInstrument[i];
       newArr.push(`${curMusician} plays ${curInstrument}`);
-    }
+    
   }
   return newArr;
 }
