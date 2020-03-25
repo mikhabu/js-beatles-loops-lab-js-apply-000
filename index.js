1d667d8b3aa8daa6289ad5c2316c02103d9bebea
Mikhail Buslau <mikhailbuslau@gmail.com>
2020-03-25 17:31:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(arrMusic, arrInstr) {
   let newArr = [];
   for (let i = 0; i < arrMusic.length; i++) {
-    
+    for (let j = 0; j < arrInstr.length; j++) {
+      newArr.push(arrMusic[i] + arrInstr[j]);
+    }
   }
 }
